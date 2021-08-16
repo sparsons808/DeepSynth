501e602d304fdd0a683b84749a42122f1f4c7430
Sydney <sydneyparsonsyb@gmail.com>
2021-08-15 22:41:20 -0400

    testing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+import * as Tone from 'tone'
+
 const Synth = require("./scripts/synth_skeleton.js");
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const el = document.querySelector("body");
 
     new Synth(el);
+
+    
+    // TESTING!!
+    // const syn = new Tone.Synth().toDestination();
+    // syn.triggerAttackRelease("C4", "8n");
+    // document.querySelector('#synth-grid').addEventListener('click',
+    // async () => {
+    //     await Tone.start();
+    //     console.log("context started");
+    // });
+   
 })
---
+++ src/scripts/sound_kits.js
@@ -0,0 +1,4 @@
+const { Tone } = require("tone/build/esm/core/Tone");
+
+const synth = new Tone.Synth().toDestination();
+
