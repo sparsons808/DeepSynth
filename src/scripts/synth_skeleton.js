4ac5ead92a7582cf288e5ba3ca7a13bd58ecf0ef
Sydney <sydneyparsonsyb@gmail.com>
2021-08-15 20:48:14 -0400

    togglesuccess

--- src/scripts/synth_skeleton.js
+++ src/scripts/synth_skeleton.js
@@ -2,22 +2,44 @@ class Synth {
     constructor (el) {
         // this.soundKit = soundKit;
         this.el = el;
+        this.soundArray = [
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,]
+        ];
         this.setupSynth();
         this.handleClick();
         console.log('constructor is firing')
     }
-
+    // handleClick toggles between activated and not activated
     handleClick() {
         const ele = document.querySelector("#synth-grid");
 
+
         ele.addEventListener("click", (event) => {
             event.preventDefault();
-            const row = event.target.parentElement.getAttribute("data-row");
-            const col = event.target.getAttribute("data-col");
+            let row = event.target.parentElement.getAttribute("data-row");
+            let col = event.target.getAttribute("data-col");
+            event.target.classList.toggle("selected");
 
-            console.log([parseInt(row), parseInt(col)]);
+            row = parseInt(row);
+            col = parseInt(col);
+
+            let value = this.soundArray[row][col];
+
+            if (value === false) {
+                value = true;
+            } else {
+                value = false;
+            };
+
+            this.soundArray[row][col] = value
+
+            console.log(this.soundArray);
         })
-        console.log(ele);
+        
     }
 
     setupSynth() {
